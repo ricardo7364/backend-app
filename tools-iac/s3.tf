ca6dd49b4934ca0bffdfccfd409cf540f69f6c57
Martin Labo <102653914+qaswqaa@users.noreply.github.com>
2023-05-11 14:21:31 +0530

    Update s3.tf

--- tools-iac/s3.tf
+++ tools-iac/s3.tf
@@ -1,141 +1,76 @@
-resource "aws_s3_bucket" "data" {
-  # bucket is public
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "${local.resource_prefix.value}-data"
-  force_destroy = true
-  tags = merge({
-    Name        = "${local.resource_prefix.value}-data"
-    Environment = local.resource_prefix.value
-    }, {
-    git_commit           = "4d57f83ca4d3a78a44fb36d1dcf0d23983fa44f5"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2022-05-18 07:08:06"
-    git_last_modified_by = "nimrod@bridgecrew.io"
-    git_modifiers        = "34870196+LironElbaz/nimrod/nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243"
-  })
-}
-
-resource "aws_s3_bucket_object" "data_object" {
-  bucket = aws_s3_bucket.data.id
-  key    = "customer-master.xlsx"
-  source = "resources/customer-master.xlsx"
-  tags = merge({
-    Name        = "${local.resource_prefix.value}-customer-master"
-    Environment = local.resource_prefix.value
-    }, {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "a7f01cc7-63c2-41a8-8555-6665e5e39a64"
-  })
-}
-
-resource "aws_s3_bucket" "financials" {
-  # bucket is not encrypted
-  # bucket does not have access logs
-  # bucket does not have versioning
-  bucket        = "${local.resource_prefix.value}-financials"
-  acl           = "private"
-  force_destroy = true
-  tags = merge({
-    Name        = "${local.resource_prefix.value}-financials"
-    Environment = local.resource_prefix.value
-    }, {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "0e012640-b597-4e5d-9378-d4b584aea913"
-  })
-
-}
-
-resource "aws_s3_bucket" "operations" {
-  # bucket is not encrypted
-  # bucket does not have access logs
-  bucket = "${local.resource_prefix.value}-operations"
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
-  force_destroy = true
-  tags = merge({
-    Name        = "${local.resource_prefix.value}-operations"
-    Environment = local.resource_prefix.value
-    }, {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "29efcf7b-22a8-4bd6-8e14-1f55b3a2d743"
-  })
-}
-
-resource "aws_s3_bucket" "data_science" {
-  # bucket is not encrypted
-  bucket = "${local.resource_prefix.value}-data-science"
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
-  logging {
-    target_bucket = "${aws_s3_bucket.logs.id}"
-    target_prefix = "log/"
-  }
+#Secret S3 Bucket
+resource "aws_s3_bucket" "aattack-path-secret-s3-bucket" {
+  bucket = "aattack-path-secret-s3-bucket-${var.ebillingid}"
   force_destroy = true
   tags = {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "9a7c8788-5655-4708-bbc3-64ead9847f64"
+      Name = "aattack-path-secret-s3-bucket-${var.ebillingid}"
+      Description = "ebilling ${var.ebillingid} S3 Bucket used for storing a secret"
+      Stack = "${var.stack-name}"
+      Scenario = "${var.scenario-name}"
   }
 }
 
-resource "aws_s3_bucket" "logs" {
-  bucket = "${local.resource_prefix.value}-logs"
-  acl    = "log-delivery-write"
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_object" "aattack-path-shepards-credentials" {
+  bucket = "${aws_s3_bucket.aattack-path-secret-s3-bucket.id}"
+  key = "admin-user.txt"
+  source = "./admin-user.txt"
+  tags = {
+    Name = "aattack-path-shepards-credentials-${var.ebillingid}"
+    Stack = "${var.stack-name}"
+    Scenario = "${var.scenario-name}"
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
-      }
-    }
-  }
+}
+
+data "aws_canonical_user_id" "current" {}
+
+resource "aws_s3_bucket" "finance-secret-s3-bucket" {
+  bucket = "finance-secret-${var.ebillingid}"
   force_destroy = true
-  tags = merge({
-    Name        = "${local.resource_prefix.value}-logs"
-    Environment = local.resource_prefix.value
-    }, {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "01946fe9-aae2-4c99-a975-e9b0d3a4696c"
-  })
+  tags = {
+      Name = "finance-secret-${var.ebillingid}"
+      Description = "ebilling ${var.ebillingid} S3 Bucket used for storing a secret"
+      Stack = "${var.stack-name}"
+      Scenario = "${var.scenario-name}"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "finance-bucket_ownership_controls" {
+  bucket = aws_s3_bucket.finance-secret-s3-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "finance-bucket_public_access_block" {
+  bucket = aws_s3_bucket.finance-secret-s3-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+resource "aws_s3_bucket_acl" "finance-bucket_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.finance-bucket_ownership_controls,
+    aws_s3_bucket_public_access_block.finance-bucket_public_access_block,
+  ]
+
+  bucket = aws_s3_bucket.finance-secret-s3-bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_object" "s3_public_objects" {
+  bucket = "${aws_s3_bucket.finance-secret-s3-bucket.id}"
+  force_destroy = true
+  tags = {
+      Name = "finance-secret-s3-bucket-${var.ebillingid}-public"
+      Description = "ebilling ${var.ebillingid} S3 Bucket used for storing a secret"
+      Stack = "${var.stack-name}"
+      Scenario = "${var.scenario-name}"
+  }
+  key = "admin-user.txt"
+  source = "./admin-user.txt"
+  content_type = "text/plain"
 }
